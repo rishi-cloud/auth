ac7092fb87807cbe018c35d4d1bf7a7bc582d9ef
rishi-cloud <rishi.singhal@zopsmart.com>
2021-10-29 15:12:11 +0530

    feat: create bundle 11

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     main: lModules,
   },
   output: {
-    filename: "bundle_10.min.js",
+    filename: "bundle_11.min.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
